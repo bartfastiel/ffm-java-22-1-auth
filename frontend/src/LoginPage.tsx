1ca2b78d6028c2d9b04e2eda63c59c3414b86889
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-09 16:22:56 +0100

    Add login page

--- frontend/src/LoginPage.tsx
+++ frontend/src/LoginPage.tsx
@@ -1,3 +1,28 @@
+import axios from "axios";
+import {useState} from "react";
+
 export default function LoginPage() {
-    return <>Login</>
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        axios.get("/api/app-users/login", {
+            auth: {
+                username,
+                password
+            }
+        })
+    }
+
+    return <>
+        <br/>
+        <label htmlFor="username">Benutzername</label>
+        <input type="text" id="username" onChange={event => setUsername(event.target.value)}/>
+        <br/>
+        <label htmlFor="password">Passwort</label>
+        <input type="password" id="password" onChange={event => setPassword(event.target.value)}/>
+        <br/>
+        <button onClick={() => login()}>Anmelden</button>
+    </>
 }
